57a473e33ea2e3b529d14946019bb8b6981513a9
servertail <adrian@servertail.com>
2017-02-21 21:45:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "<h1>Hello2 from OSC7!</h1>";
-echo "<h2>Checking for docker images</h2>";
+echo "<h1>Hello from OCP 3.4!</h1>";
+echo "<h2>Testing the latest OpenShift Origin 1.4 (RedHat's OCP 3.4)</h2>";
 ?>
